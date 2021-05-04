0e5d2d3679488e9d68a8ddc4750ccb1e6917e835
MANT-i-S <mantis.s95@gmail.com>
2021-05-04 15:38:53 -0700

    Still playing with quartz

--- Set/PlayingCardView.swift
+++ Set/PlayingCardView.swift
@@ -10,16 +10,23 @@ import UIKit
 class PlayingCardView: UIView {
     
     override func draw(_ rect: CGRect) {
-        let midOfMidY = bounds.midY / 2
+        let shade = 2
         if let context = UIGraphicsGetCurrentContext() {
             context.move(to: CGPoint (x: bounds.midX / 2, y: bounds.midY))
             context.addLine(to: CGPoint(x: bounds.midX, y: bounds.midY * 0.75))
             context.addLine(to: CGPoint(x: bounds.maxX * 0.75, y: bounds.midY))
             context.addLine(to: CGPoint(x: bounds.midX, y: bounds.midY + bounds.midY * 0.25))
             context.addLine(to: CGPoint (x: bounds.midX / 2, y: bounds.midY))
-            UIColor.green.setStroke()
-            context.strokePath()
-            setNeedsDisplay()
+            if shade == 1 {
+                //TODO make color variable.
+                UIColor.green.setStroke()
+                context.strokePath()
+            } else if shade == 2 {
+                UIColor.red.setFill()
+                context.fillPath()
+            } else if shade == 3 {
+                //TODO figure out how to create effect of shade
+            }
         }
     }
 }
