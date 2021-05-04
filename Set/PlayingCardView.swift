79f375dac6cbf887ac8b3ee038c4674f18678001
MANT-i-S <mantis.s95@gmail.com>
2021-05-04 16:20:35 -0700

    Using path for clipping

--- Set/PlayingCardView.swift
+++ Set/PlayingCardView.swift
@@ -10,23 +10,29 @@ import UIKit
 class PlayingCardView: UIView {
     
     override func draw(_ rect: CGRect) {
-        let shade = 2
-        if let context = UIGraphicsGetCurrentContext() {
-            context.move(to: CGPoint (x: bounds.midX / 2, y: bounds.midY))
-            context.addLine(to: CGPoint(x: bounds.midX, y: bounds.midY * 0.75))
-            context.addLine(to: CGPoint(x: bounds.maxX * 0.75, y: bounds.midY))
-            context.addLine(to: CGPoint(x: bounds.midX, y: bounds.midY + bounds.midY * 0.25))
-            context.addLine(to: CGPoint (x: bounds.midX / 2, y: bounds.midY))
-            if shade == 1 {
-                //TODO make color variable.
-                UIColor.green.setStroke()
-                context.strokePath()
-            } else if shade == 2 {
-                UIColor.red.setFill()
-                context.fillPath()
-            } else if shade == 3 {
-                //TODO figure out how to create effect of shade
-            }
+        let shade = 1
+        let path = UIBezierPath()
+        path.move(to: CGPoint (x: bounds.midX / 2, y: bounds.midY))
+        path.addLine(to: CGPoint(x: bounds.midX, y: bounds.midY * 0.75))
+        path.addLine(to: CGPoint(x: bounds.maxX * 0.75, y: bounds.midY))
+        path.addLine(to: CGPoint(x: bounds.midX, y: bounds.midY + bounds.midY * 0.25))
+        path.addLine(to: CGPoint (x: bounds.midX / 2, y: bounds.midY))
+        path.addClip()
+        path.addLine(to: CGPoint (x: bounds.maxX, y: bounds.midY))
+        if shade == 1 {
+            //TODO make color variable.
+            UIColor.green.setStroke()
+            path.stroke()
+        } else if shade == 2 {
+            UIColor.red.setFill()
+            path.fill()
+        } else if shade == 3 {
+            //TODO figure out how to create effect of shade
         }
+    }
+    
+    func addShading()
+    {
+        
     }
 }
